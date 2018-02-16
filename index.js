96107ce2372163f394b89104092310c0dfd0f6a0
Lisa Chan <xlisachan@gmail.com>
2018-02-16 02:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,56 @@ function theBeatlesPlay(musicians, instruments){
   return newArray;
 }
 
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
-  var i = 0
-  while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
-    i++;
+function johnLennonFacts() {
+  var newArray = [];
+  var johnLennonFactsIndex = 0;
+  while (johnLennonFactsIndex < johnLennonFacts.length) {
+    var johnLennonSentence = johnLennonFacts[johnLennonFactsIndex] + "!!!";
+    newArray.push(johnLennonSentence);
+    johnLennonFactsIndex++;
   }
-  return newFacts;
+  return newArray;
+}
+it("returns an array of strings containing what instruments each musician plays", function() {
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+  expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
+});
+
+describe('johnLennonFacts', function(){
+  it("returns an array of strings with exclamation points", function() {
+    expect(johnLennonFacts([
+      "He was the last Beatle to learn to drive",
+      "He was never a vegetarian",
+      "He was a choir boy and boy scout",
+      "He hated the sound of his own voice"
+    ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+
+    expect(johnLennonFacts([
+      "foo",
+      "bar",
+    ])).toEqual(["foo!!!", "bar!!!"])
+  });
+});
+
+describe('iLoveTheBeatles', function() {
+  it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+    expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+  });
+
+  it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+    expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+  });
+});
+
+});
+
+
+function iLoveTheBeatles(number){
+  var [];
+  do{}
+  while{number < 15}
+  return 'I love the Beatles!';
+}
 }
