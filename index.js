6b7bc669d8fe150f016865f432ca2b333c6c99ea
Lisa Chan <xlisachan@gmail.com>
2018-02-16 02:12:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function theBeatlesPlay(musicians, instruments){
   return newArray;
 }
 
-function johnLennonFacts() {
-  var johnLennonArray = [];
-  var johnLennonFactsIndex = 0;
-  while (johnLennonFactsIndex < johnLennonFacts.length) {
-    var johnLennonSentence = johnLennonFacts[johnLennonFactsIndex] + "!!!";
-    johnLennonArray.push(johnLennonSentence);
-    johnLennonFactsIndex++;
+function johnLennonFacts(){
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
   }
-  return johnLennonArray;
+  return newFacts;
 }
