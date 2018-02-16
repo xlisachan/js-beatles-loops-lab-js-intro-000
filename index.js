4d2a8100dd6ef8139ec9c8e19a07f94df7a572d3
Lisa Chan <xlisachan@gmail.com>
2018-02-16 02:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ function iLoveTheBeatles(number) {
   do {
     newArray.push("I love the Beatles!");
     i++;
-  } while(i < number && i < 15);
+  } while(i < number || i < 15);
   return newArray;
 }
+
+describe('iLoveTheBeatles', function() {
+  it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
+    expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+  });
+
+  it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+    expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
+  });
+});
+
+});
